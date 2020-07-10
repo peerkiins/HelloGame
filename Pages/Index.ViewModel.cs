e8564c8740a4a560565a2a75d62031d2bc2fb2ac
Perkins Petter Ong <perkinspetter@outlook.com>
2020-07-10 19:09:05 +0800

    Movements, Passing Walls

--- Pages/Index.ViewModel.cs
+++ Pages/Index.ViewModel.cs
@@ -7,38 +7,90 @@ using Microsoft.AspNetCore.Components.Web;
 
 namespace HelloGame
 {
-    public class IndexViewModel: ComponentBase
+    public class IndexViewModel : ComponentBase
     {
         private Canvas2DContext _context;
 
         protected BECanvasComponent _canvasReference;
         protected ElementReference ashRef;
+        int maxFrames = 4;
+        int currentFrame = 1;
+        int mapBorderMaxY = 768;
+        int mapBorderMaxX = 1024;
+        int spriteX = 64;
+        int spriteY = 0;
+        int y = 352;
+        int x = 480;
+        int motion;
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            if(firstRender)
+            if (firstRender)
             {
                 this._context = await this._canvasReference.CreateCanvas2DAsync();
-                int maxFrames = 4;
-                int currentFrame = 1;
-                int y = 0;
-                while(true)
-                {
-                    await Task.Delay(100);
-                    await this._context.SetFillStyleAsync("cornflowerblue");
-                    await this._context.FillRectAsync(0, 0, 1024, 768);
-                    await this._context.DrawImageAsync(ashRef, (currentFrame - 1) * 64, 0, 64, 64, 0, y, 64, 64);
-                    currentFrame++;
-                    y++;
-                    if(currentFrame > maxFrames)
-                        currentFrame = 1;
-                }
+                await Task.Delay(100);
+                await this._context.SetFillStyleAsync("cornflowerblue");
+                await this._context.FillRectAsync(0, 0, mapBorderMaxX, mapBorderMaxY);
+                await this._context.DrawImageAsync(ashRef, (currentFrame - 1) * spriteX, spriteY, 64, 64, x, y, 64, 64);
+                if (currentFrame > maxFrames)
+                    currentFrame = 1;
+            }
+            else
+            {
+                await Task.Delay(100);
+                await this._context.SetFillStyleAsync("cornflowerblue");
+                await this._context.FillRectAsync(0, 0, mapBorderMaxX, mapBorderMaxY);
+                await this._context.DrawImageAsync(ashRef, (currentFrame - 1) * spriteX, spriteY, 64, 64, x, y, 64, 64);
+                if (currentFrame > maxFrames)
+                    currentFrame = 1;
             }
         }
 
         protected void OnKeypress(KeyboardEventArgs e)
         {
-            Console.WriteLine(e.Code);
+            switch (e.Key)
+            {
+                case "w":
+                case "W":
+                    spriteY = 192;
+                    y--;
+                    currentFrame++;
+                    if (y < 0)
+                    {
+                        y = mapBorderMaxY;
+                    }
+                    break;
+                case "a":
+                case "A":
+                    spriteY = 64;
+                    x--;
+                    currentFrame++;
+                    if (x < 0)
+                    {
+                        x = mapBorderMaxX;
+                    }
+                    break;
+                case "s":
+                case "S":
+                    spriteY = 0;
+                    y++;
+                    currentFrame++;
+                    if (y > mapBorderMaxY)
+                    {
+                        y = 0;
+                    }
+                    break;
+                case "d":
+                case "D":
+                    spriteY = 128;
+                    x++;
+                    currentFrame++;
+                    if (x > mapBorderMaxX)
+                    {
+                        x = 0;
+                    }
+                    break;
+            }
         }
     }
 }
